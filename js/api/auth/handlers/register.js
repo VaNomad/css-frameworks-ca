3405442466d9d3d814e4c61711a13982ebb53d95
VaNomad <sjur.hassel@gmail.com>
2022-12-05 19:30:47 +0000

    corrected button alignment, trying to fix form eventlistener

--- js/api/auth/handlers/register.js
+++ js/api/auth/handlers/register.js
@@ -18,22 +18,22 @@
 
 const form = document.querySelector("#registerForm");
 
-form.addEventListener("submit", (event) => {
-    event.preventDefault();
-    console.log(form);
+function registerForm() {
+    form.addEventListener("submit", (event) => {
+        event.preventDefault();
+        console.log(form);
+        
+        const form = event.target;
+        
+        const formData = new FormData(form);
+        const profile = Object.fromEntries(formData.entries());
+        console.log(profile);
+        
     
-    const form = event.target;
-    
-    const formData = new FormData(form);
-    const profile = Object.fromEntries(formData.entries());
-    console.log(profile);
-    
-
-    
-});
+        
+    });
+}
 
 
-console.log('hello');
-
-
+registerForm();
 
