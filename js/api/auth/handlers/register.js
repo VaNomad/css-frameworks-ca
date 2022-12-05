b6006458e65ec05c66d2165f41abb88f5208eac8
VaNomad <sjur.hassel@gmail.com>
2022-12-05 16:19:51 +0000

    adding eventlistener to registry form

---
+++ js/api/auth/handlers/register.js
@@ -0,0 +1,39 @@
+
+// import { API_BASE_URL } from "./constants.mjs";
+
+// export async function register(url, profile, method) {
+    
+//     const response = await fetch(url, {
+//         method: method,
+//         headers: {
+//             "Content-Type": "application/json",
+//         },
+
+//         body: JSON.stringify(profile),
+//     });
+
+//     const result = await response.json()
+
+// }
+
+const form = document.querySelector("#registerForm");
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    console.log(form);
+    
+    const form = event.target;
+    
+    const formData = new FormData(form);
+    const profile = Object.fromEntries(formData.entries());
+    console.log(profile);
+    
+
+    
+});
+
+
+console.log('hello');
+
+
+
