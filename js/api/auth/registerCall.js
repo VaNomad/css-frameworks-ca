144d2bb968ed5a262ef6eb86ddacb51500099c1a
VaNomad <sjur.hassel@gmail.com>
2022-12-15 10:55:54 +0000

    added to registerCall + jsdocs

--- js/api/auth/registerCall.js
+++ js/api/auth/registerCall.js
@@ -1,29 +1,43 @@
-import { API_SOCIAL_URL } from "../../constants.js";
+import {
+  API_SOCIAL_URL
+} from "../../constants.js";
 
 export const registerURL = `${API_SOCIAL_URL}/auth/register`;
 console.log(registerURL);
 
 const method = "post";
 
+/**
+ * API call that registeres a new user
+ * @param {string} registerURL
+ * @param {string} profile 
+ * ```js
+ * registerNewUser(registerURL, profile)
+ * ```
+ */
 export async function registerNewUser(profile) {
   try {
 
-  const body = JSON.stringify(profile);
+    const body = JSON.stringify(profile);
+
+    const response = await fetch(registerURL, {
+      headers: {
+        "Content-Type": "application/json"
+      },
+      method,
+      body
+    })
+    if (response.ok) {
+      alert("Success! Your account is registered!")
+    }
     
-  const response = await fetch(registerURL, {
-    headers: {
-      "Content-Type": "application/json"
-    },
-    method,
-    body
-  })
-  
-  const result = await response.json();
-  console.log(result);
-    
+
+    const result = await response.json();
+    console.log(result);
+
   } catch (error) {
-    console.log(error, "The API call failed");
+    alert(error, "The API call failed");
   }
+  // return (result)
 }
 registerNewUser(registerURL);
-
