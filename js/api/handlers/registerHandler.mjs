c965bce90bedd23b1bf612ff1ec75a26dc888354
VaNomad <sjur.hassel@gmail.com>
2022-12-31 10:35:45 +0000

    updated register message

--- js/api/handlers/registerHandler.mjs
+++ js/api/handlers/registerHandler.mjs
@@ -21,8 +21,9 @@ import { displayError } from "../../ui/displayError.mjs";
 
 
 
- export function registerFormListener() {
+ export async function registerFormListener() {
     const registryForm = document.querySelector("#registerForm");
+    const regMsg = document.querySelector("#regMsg");
 
     registryForm.addEventListener("submit", async (event) => {
         event.preventDefault();
@@ -31,14 +32,40 @@ import { displayError } from "../../ui/displayError.mjs";
         const profile = Object.fromEntries(formData.entries());
         console.log(profile);
 
-        try {
-            await registerNewUser(profile);
-            if (profile.ok) {
-                window.location = "/login.html";  
-            } 
-        } catch (error) {
+        // try {
+        //     await registerNewUser(profile);
+        //     if (profile.ok) {
+        //         form.reset();
+        //         window.location = "/login.html";
+        //     }
+        // } catch (error) {
+        //     displayError(registryForm, error);
+        // }
+        
+        setTimeout(() => {
+            regMsg.style.display = "none";
+          }, 7000);
+          // form.reset();
+          if (registryForm.password.value && registryForm.name.value && registryForm.email.value) {
+              regMsg.innerHTML = `
+                                        <div class="card border-success mx-auto" style="min-width: 50rem;">
+                                        <div class="card-header text-info">Success!</div>
+                                        <div class="card-body">
+                                          <h5 class="card-title lead">Your account is registered!</h5>
+                                          <p class="card-title">Head on over to the login page to enter Demo Tape.</p>
+                                        </div>`
+                                        form.reset();
+                                        
+                                        setTimeout(() => {
+                                            window.location = "/login.html";;
+                                          }, 7500);
+                                        
+            
+          } else {
             displayError(registryForm, error);
-        }
+          }
+        
+        
         
     });
 };
