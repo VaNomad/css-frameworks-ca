5b679099d8bdc6719293db84a5c96bb8e7c5ec08
VaNomad <sjur.hassel@gmail.com>
2023-01-24 07:46:42 +0000

    Adds error msg on login

--- js/api/handlers/loginHandler.mjs
+++ js/api/handlers/loginHandler.mjs
@@ -3,6 +3,7 @@ import { displayError } from "../../ui/displayError.mjs";
 
 export function loginFormListener() {
   const loginForm = document.querySelector("#loginForm");
+  const regMsg = document.querySelector("#regMsg");
 
   loginForm.addEventListener("submit", async (event) => {
     event.preventDefault();
@@ -17,6 +18,22 @@ export function loginFormListener() {
       if (response.ok) {
         window.location.replace("/profile.html");
         return response
+      } else {
+        regMsg.innerHTML = `
+                                      <div class="card border-success mx-auto" style="min-width: 50rem;">
+                                      <div class="card-header text-info">No Entry!</div>
+                                      <div class="card-body">
+                                        <h5 class="card-title lead">Your account is not registered</h5>
+                                        <p class="card-title">Click "Create New Account" button to register</p>
+                                      </div>`
+                                      form.reset();
+                                      
+                                      setTimeout(() => {
+                                          window.location = "/login.html";;
+                                        }, 7500);
+                                      
+          
+        
       }
     } catch (error) {
       displayError(loginForm, error);
